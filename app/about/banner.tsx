0775ef3eae51c839ff563224e55bec7f64721f1e
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2024-03-26 20:26:26 +0600

    updated

--- app/about/banner.tsx
+++ app/about/banner.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 const Banner = (props: Props) => {
 	return (
-		<div className="lg:bg-[url('../assets/bgimage.png')] bg-cover w-full lg:h-full bg-center bg-no-repeat">
+		<div className="lg:bg-[url('../assets/bgimage.png')] hidden md:block bg-cover w-full lg:h-full bg-center bg-no-repeat">
 			<h1 className='text-center text-4xl font-extrabold py-24'>
 				Online Learning whearever and whenever
 			</h1>
--- app/about/serviceOptions.tsx
+++ app/about/serviceOptions.tsx
@@ -9,45 +9,45 @@ type Props = {};
 
 const ServiceOptions = (props: Props) => {
 	return (
-		<div className='lg:w-10/12 2xl:max-w-screen-xl mx-auto'>
-			<div className='flex items-center justify-center py-10'>
-				<div className='border-r-2 border-b-2 '>
+		<div className='lg:w-10/12 2xl:max-w-screen-xl mx-auto px-5 lg:px-0'>
+			<div className='flex flex-wrap items-center justify-center py-10'>
+				<div className='lg:border-r-2 lg:border-b-2 '>
 					<div className='w-72 flex flex-col items-center justify-center  text-center gap-y-5 px-10 pb-10'>
 						<Image src={Learning} alt='' width={150} />
 						<h3 className='text-lg font-semibold'>Skiled instrutors</h3>
 						<p>Best online platform for professional courses</p>
 					</div>
-					<div className='text-center py-3 border-t-2'>
+					<div className='text-center py-3 border-t-2 hidden lg:block'>
 						<p className='uppercase font-semibold'>Student Enrolled</p>
 					</div>
 				</div>
-				<div className='border-r-2 border-b-2 '>
-					<div className='w-72 flex flex-col items-center justify-center  text-center gap-y-5 px-10 pb-10'>
+				<div className='lg:border-r-2 lg:border-b-2 '>
+					<div className='w-72 flex flex-col items-center justify-center text-center gap-y-5 px-10 pb-10'>
 						<Image src={Idea} alt='' width={150} />
 						<h3 className='text-lg font-semibold'>Eductor helps</h3>
 						<p>Best online platform for professional courses</p>
 					</div>
-					<div className='text-center py-3 border-t-2'>
+					<div className='text-center py-3 border-t-2 hidden lg:block'>
 						<p className='uppercase font-semibold'>Student Enrolled</p>
 					</div>
 				</div>
-				<div className='border-r-2 border-b-2 '>
+				<div className='lg:border-r-2 lg:border-b-2 '>
 					<div className='w-72 flex flex-col items-center justify-center  text-center gap-y-5 px-10 pb-10'>
 						<Image src={Certify} alt='' width={150} />
 						<h3 className='text-lg font-semibold'>Get certificate</h3>
 						<p>Best online platform for professional courses</p>
 					</div>
-					<div className='text-center py-3 border-t-2'>
+					<div className='text-center py-3 border-t-2 hidden lg:block'>
 						<p className='uppercase font-semibold'>Student Enrolled</p>
 					</div>
 				</div>
-				<div className='border-b-2'>
+				<div className='lg:border-b-2'>
 					<div className='w-72 flex flex-col items-center justify-center  text-center gap-y-5 px-10 pb-10'>
 						<Image src={OnlineClass} alt='' width={150} />
 						<h3 className='text-lg font-semibold'>Online classes</h3>
 						<p>Best online platform for professional courses</p>
 					</div>
-					<div className='text-center py-3 border-t-2'>
+					<div className='text-center py-3 border-t-2 hidden lg:block'>
 						<p className='uppercase font-semibold'>Student Enrolled</p>
 					</div>
 				</div>
