e265615abb0da12209c126c5e7685ae0a5385f0a
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2024-03-26 20:14:31 +0600

    Update hero.tsx

--- app/about/hero.tsx
+++ app/about/hero.tsx
@@ -5,13 +5,13 @@ const Hero = () => {
 	return (
 		<main id='home' className='w-full lg:h-[50rem]'>
 			<div className='h-full w-full pt-20 flex items-center justify-center'>
-				<div className='lg:w-10/12 2xl:max-w-screen-xl mx-auto grid grid-cols-1 lg:grid-cols-2 content-center'>
+				<div className='lg:w-10/12 2xl:max-w-screen-xl mx-auto grid grid-cols-1 lg:grid-cols-2 content-center px-5 lg:px-0'>
 					<div className='flex items-center'>
 						<div className='text-center md:text-left mb-5'>
-							<h1 className='text-5xl lg:text-[48px] text-[#0F172A] font-bold leading-snug'>
+							<h1 className='text-3xl lg:text-5xl lg:text-[48px] text-[#0F172A] font-bold leading-snug'>
 								Unlock Your Potential with Unlimited Learning Possibilities
 							</h1>
-							<h3 className='my-10 text-[#666666] text-lg'>
+							<h3 className='my-10 text-[#666666] text-sm lg:text-lg'>
 								Our interactive platform offers diverse courses tailored to
 								different skill levels and interests. We provide personalized
 								support, mentorship, and feedback to empower students to achieve
