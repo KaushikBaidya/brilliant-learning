affd1e067fafa7d99b5f7b4401a9e1bfa45d407d
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2024-03-26 20:20:01 +0600

    Update ourService.tsx

--- app/about/ourService.tsx
+++ app/about/ourService.tsx
@@ -5,18 +5,18 @@ type Props = {};
 
 const OurService = (props: Props) => {
 	return (
-		<div className='lg:w-10/12 2xl:max-w-screen-xl mx-auto'>
+		<div className='lg:w-10/12 2xl:max-w-screen-xl mx-auto px-5 lg:px-0'>
 			<div className='flex items-center gap-5'>
-				<div className='w-10 h-10 bg-[#6366F1] text-white flex items-center justify-center rounded-full'>
+				<div className='w-6 lg:w-10 h-6 lg:h-10 bg-[#6366F1] text-white flex items-center justify-center rounded-full p-1'>
 					<TfiWorld size={25} />
 				</div>
-				<h3 className='text-xl font-medium'>Know about classes</h3>
+				<h3 className='text-sm lg:text-xl font-medium'>Know about classes</h3>
 			</div>
-			<div className='w-full grid grid-cols-2 py-10'>
-				<h1 className='text-5xl font-bold'>
+			<div className='w-full grid grid-cols-1 md:grid-cols-2 gap-2  py-5 lg:py-10'>
+				<h1 className='text-3xl lg:text-5xl font-bold text-gray-800 '>
 					Delivering top-quality courses to our students
 				</h1>
-				<div className='text-lg font-medium'>
+				<div className='text-sm lg:text-lg font-medium'>
 					<p>
 						We provide personalized support to our students every step of the
 						way
