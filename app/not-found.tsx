40411b75a7260293363d8a1c4aa7ed29b2afad57
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2024-10-02 14:40:08 +0600

    temporary solution for layouting

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
-import ErrorImage from '@/assets/errorpage.gif';
-import Image from 'next/image';
+"use client";
+import React from "react";
+import ErrorImage from "@/assets/errorpage.gif";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import "./notfound.css";
 
 type Props = {};
 
 const NotFound = (props: Props) => {
-	return (
-		<div className='w-full h-screen flex items-center justify-center'>
-			<Image src={ErrorImage} alt='404 - page not found' />
-		</div>
-	);
+  const router = useRouter();
+
+  const goHome = () => {
+    router.push("/");
+  };
+
+  return (
+    <div className="not-found-page">
+      <Image
+        src={ErrorImage}
+        alt="404 - page not found"
+        className="not-found-image"
+      />
+      <button className="go-home-button" onClick={goHome}>
+        Go Home
+      </button>
+    </div>
+  );
 };
 
 export default NotFound;
